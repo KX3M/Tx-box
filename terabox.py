3b4b669ef54e4857944793c1f63a2ce08e6deb1b
folieds <yosnkooke@gmail.com>
2025-02-02 00:46:12 +0530

    Update terabox.py

--- terabox.py
+++ terabox.py
@@ -58,7 +58,7 @@ else:
     fsub_id = int(fsub_id)
 
 
-mongo_url = os.environ.get('MONGO_URL', 'mongodb+srv://hegodal811:rsRu17pspZAcp6V7@cluster0.prsvqax.mongodb.net/?retryWrites=true&w=majority')
+mongo_url = os.environ.get('MONGO_URL', 'mongodb+srv://Outlawbots:Zoro@cluster0.huekk.mongodb.net/?retryWrites=true&w=majority')
 client = MongoClient(mongo_url)
 db = client['cphdlust']
 users_collection = db['users']
